d6ee55cde44020823415e09221d832bda78c6ab1
pepe <davidpeewee01241@outlook.com>
2025-05-13 13:34:50 -0500

    java clases

---
+++ 13-05-2025/AprendizajeJAVA/src/aprendizajejava/AprendizajeJAVA.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aprendizajejava;
+import java.util.Scanner;
+/**
+ *
+ * @author Usuario
+ */
+public class AprendizajeJAVA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner  entrada = new Scanner (System.in, "ISO-8859-1");
+        int edad;
+        String nombre;
+        String apellido;
+        System.out.println("HOLA");
+        System.out.println("¿Cual es tu nombre?");
+        nombre = entrada.next();
+        System.out.println("Y tu apellido");
+        apellido = entrada.next();
+        System.out.println("DIGITA TU EDAD");
+        edad = entrada.nextInt();
+        int decada = edad - 10;
+        System.out.println(nombre + ' ' + apellido + " tu edad es: " + edad);        // TODO code application logic here
+        System.out.println("Hace una decada tenias " + decada);
+    }
+    
+}
