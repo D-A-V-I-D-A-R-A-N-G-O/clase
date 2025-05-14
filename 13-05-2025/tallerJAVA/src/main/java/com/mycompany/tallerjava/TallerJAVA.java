4fb3a707b6bdb783d330cda3ce245d41b8481f3b
pepe <davidpeewee01241@outlook.com>
2025-05-13 19:53:47 -0500

    taller

---
+++ 13-05-2025/tallerJAVA/src/main/java/com/mycompany/tallerjava/TallerJAVA.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tallerjava;
+import java.util.Scanner;
+/**
+ *
+ * @author peewee
+ */
+public class TallerJAVA {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in, "ISO-8859-1");
+        double num1, num2 ;
+         int operador;
+        System.out.println("Hola Usuario, selecciona el numero de alguna de las opciones para operar");
+        System.out.println("1-suma:2-resta:3-multiplicar:4-dividir:5-mayor igual o menor que");
+        operador = entrada.nextInt();
+        System.out.println("ingresa los 2 numeros a operar");
+        num1= entrada.nextInt();
+        num2= entrada.nextInt();
+        
+        switch (operador){
+            case 1 :
+                               System.out.println("El resultado es: " + (num1+num2));
+
+            break;
+            case 2 :
+                               System.out.println("El resultado es: " + (num1-num2));
+
+            break;
+            case 3 :
+                               System.out.println("El resultado es: " + (num1*num2));
+
+            break;
+            case 4 :
+                if(num2 == 0){
+                    System.out.println("Esta división no es posible, ingresa un numero divisor diferente del 0");
+                }else{
+                                                   System.out.println("El resultado es: " + (num1/num2));
+                }
+            break;
+            case 5:
+                if(num1 == num2){
+                    System.out.println("son iguales");
+                }
+                if(num1 > num2){
+                    System.out.println("El "+ num1 + " es mayor que el "+ num2);
+                }else {
+                    System.out.println("El " + num2 + " es mayo que el "+ num1);
+                }
+                break;
+            default:
+                System.out.println("repite el programa e ingresa un numero u operador valido");
+        }
+        
+    }
+}
