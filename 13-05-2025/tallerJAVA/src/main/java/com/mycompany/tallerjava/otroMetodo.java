4b2b52ccdbe5db0be0d9e12c51597cc1888cd589
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2025-05-16 23:54:14 -0500

    f

---
+++ 13-05-2025/tallerJAVA/src/main/java/com/mycompany/tallerjava/otroMetodo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tallerjava;
+import java.util.Scanner;
+
+/**
+ *
+ * @author david
+ */
+public class otroMetodo {
+    public static void main(String[] args) {
+                    Scanner entrada = new Scanner (System.in, "ISO-8859-1");
+                    System.out.println("ingresa el operador");
+    }
+}
